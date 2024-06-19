ebbb1e4a1e43224b42d2498aad6946abf9cf4ce1
VictorHugo <109436503+Victor20021223@users.noreply.github.com>
2024-06-19 12:49:05 -0300

    Envio Final

--- App.js
+++ App.js
@@ -1,16 +1,7 @@
 // App.js
 import React from "react";
 import IntroStack from "../Controlador_agua_App/navigation/IntroStack";
-import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
-import SetupScreen from "./screens/SetupScreen";
-import MetaScreen from "./screens/MetaScreen";
-import NotificationPage from "./components/NotificationPage";
-import GoalsPage from "./components/GoalsPage";
-import SettingsScreen from "./screens/SettingsScreen";
 
 
 const App = () => {
